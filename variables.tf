224ded72a0443ae33f6afc7737c81728b3ae2ff8
reuben-james <32489022+reuben-james@users.noreply.github.com>
2021-08-24 07:32:54 +0100

    Update to variables.tf

--- variables.tf
+++ variables.tf
@@ -10,6 +10,6 @@ variable "instance_type" {
 
 variable "instance_name" {
   description = "EC2 instance name"
-  default     = "Updated by Terraform"
+  default     = "Updated by Terraform - TESTING"
 }
 
